5daf4f613ad6622ac5eb20a6d3b73fddf3a2a380
ClemF4 <clementfau4@gmail.com>
2026-02-16 16:25:32 +0000

    Github actions fix 2

--- custom_components/cattainer_integration/const.py
+++ custom_components/cattainer_integration/const.py
@@ -4,5 +4,5 @@ from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-DOMAIN = "custom_components/cattainer_integration"
+DOMAIN = "cattainer_integration"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
